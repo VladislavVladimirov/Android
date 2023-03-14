7652b6b6addbc17cfb7058ce9a956b7fc65334ec
VladislavVladimirov <vladikvladimirov@gmail.com>
2023-03-15 02:26:27 +0300

    add recyclerView

--- app/src/main/java/com/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/netology/nmedia/viewmodel/PostViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import com.netology.nmedia.repository.PostRepository
 import com.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
-class PostViewModel: ViewModel() {
+class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
-    val data = repository.get()
-    fun like() = repository.like()
-    fun share() = repository.share()
+    val data = repository.getAll()
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
 
-    }
+}
