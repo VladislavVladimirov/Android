2b9020bb65303dd0759566674017b8112d1da317
VladislavVladimirov <vladikvladimirov@gmail.com>
2023-04-12 19:17:47 +0300

    broken add and edit

--- app/src/main/java/com/netology/nmedia/activity/NewPostResultContract.kt
+++ app/src/main/java/com/netology/nmedia/activity/NewPostResultContract.kt
@@ -7,9 +7,7 @@ import androidx.activity.result.contract.ActivityResultContract
 
 class NewPostResultContract : ActivityResultContract<Unit, String?>() {
 
-    override fun createIntent(context: Context, input: Unit): Intent =
-        Intent(context, NewPostActivity::class.java)
-
+    override fun createIntent(context: Context, input: Unit): Intent = Intent(context,NewPostActivity::class.java)
     override fun parseResult(resultCode: Int, intent: Intent?): String? =
         if (resultCode == Activity.RESULT_OK) {
             intent?.getStringExtra(Intent.EXTRA_TEXT)
