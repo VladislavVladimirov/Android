6e1d41de74f75c8290e0e4482aad83c6b3290e47
VladislavVladimirov <vladikvladimirov@gmail.com>
2023-06-07 01:56:35 +0300

    Migrate all functionality on Retrofit

--- app/src/main/java/com/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/com/netology/nmedia/repository/PostRepository.kt
@@ -4,14 +4,16 @@ import com.netology.nmedia.dto.Post
 
 interface PostRepository {
 
-    fun likeById(post:Post, callback: PostsCallback<Post>)
+    fun likeById(id: Long, callback: PostsCallback<Post>)
+    fun dislikeById(id: Long, callback: PostsCallback<Post>)
     fun removeById(id: Long, callback: PostsCallback<Unit>)
     fun save(post: Post, callback: PostsCallback<Post>)
-    fun getAllAsync(callback: PostsCallback<List<Post>>)
+    fun getAll(callback: PostsCallback<List<Post>>)
 
     interface PostsCallback <T> {
         fun onSuccess(input: T) {}
         fun onError(e: Exception) {}
+
     }
 
 
